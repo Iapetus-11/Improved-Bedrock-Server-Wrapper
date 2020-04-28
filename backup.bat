0c4fd48a0d0c953dc6e95ed77e5da357a770412b
Iapetus-11 <olimdahacker@gmail.com>
2020-04-28 11:06:03 -0400

    make log file be backed up too

--- backup.bat
+++ backup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:time
+:: Weird time shit I forgot why it's like this but there's probably a good reason so lets just keep it this way nice mate
 date /t > date.txt
 set date=<date.txt
 set date=%date:/=.%
@@ -10,11 +10,12 @@ set time=<time.txt
 set time=%time::=.%
 del time.txt
 
-:mbd
+:: Make the directory if it doesn't exist
 mkdir "backups\%date% %time%"
 
-:backupdirs
+:: actually backup the stuff
 xcopy /q /e /i "worlds\world" "backups\%date% %time%\world\"
+xcopy /q /e /i "log.txt" "backups\%date% %time%\log.txt"
 
 echo.
 echo %date% %time%: Backup Completed!
